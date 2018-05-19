2052deafb526379e7f0d8f337df6e1008defe1c7
Revel Yusuf <crazyprogrammer109@gmail.com>
2018-05-19 12:44:14 +0600

    Rest others

---
+++ new.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+
+
+<?php 
+
+
+
+
+echo "<h1> Hello </h1>";
+
+
+
+<form method="post" action="hostgator.html">  
+<input type="input" name="submit" value="Submit"> 
+</form>
+
+ ?>
+
+</body>
+</html>
